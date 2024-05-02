507132e6771495e0216c757fc1861857eff9b795
Anuj Kapse <anujkapse26jan@gmail.com>
2024-05-03 02:18:57 +0530

    Carousel

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
+window.onload = function() {
+    var fadeInElement = document.querySelector('.TopContainer');
+    fadeInElement.style.display = "none"; // Hide the element initially
+    fadeInElement.style.display = "block"; // Show the element
+};
 let slideIndex = 0;
-showSlide(slideIndex);
 
-function prevSlide() {
-    showSlide(slideIndex -= 1);
+function showSlide(index) {
+  const slides = document.querySelectorAll('.slide');
+  if (index >= slides.length) { slideIndex = 0; }
+  if (index < 0) { slideIndex = slides.length - 1; }
+  slides.forEach(slide => slide.style.display = 'none');
+  slides[slideIndex].style.display = 'block';
 }
 
 function nextSlide() {
-    showSlide(slideIndex += 1);
+  showSlide(++slideIndex);
 }
 
-function showSlide(n) {
-    let slides = document.getElementsByClassName("carousel-images")[0].getElementsByTagName("img");
-    if (n >= slides.length) {slideIndex = 0}
-    if (n < 0) {slideIndex = slides.length - 1}
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex].style.display = "block";
+function prevSlide() {
+  showSlide(--slideIndex);
 }
+
+// Show the initial slide
+showSlide(slideIndex);
